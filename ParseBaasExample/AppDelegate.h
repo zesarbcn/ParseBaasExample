22e2bf4125ee2badc73f3bf284fbfd296db99a32
Cesar Perez <kaesarbcn@hotmail.com>
2014-03-29 20:17:13 +0100

    Initial Commit

---
+++ ParseBaasExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseBaasExample
+//
+//  Created by Cesar Perez Laguna on 29/03/14.
+//  Copyright (c) 2014 Cesar Perez Laguna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
